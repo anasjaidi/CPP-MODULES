92aef11f2ce68987a164776d56681a8a55741328
anas jaidi <anas.jaidi@icloud.com>
2022-10-10 19:43:33 +0100

    delete: ex01/module_00 to update

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main(int ac, char **av)
+{
+  std::string str;
+  int i = 0;
+  if (ac == 1)
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+  while (++i < ac)
+  {
+    str = av[i];
+    for (std::string::iterator it = str.begin(); it != str.end(); it++)
+      std::cout << (char)std::toupper(*it);
+  }
+  std::cout << std::endl;
+  return (0);
+}
