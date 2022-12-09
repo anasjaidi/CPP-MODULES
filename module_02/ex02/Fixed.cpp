c20fabffd4218cbdb000714a461708915029d9a8
Anas Jaidi <ajaidi@e2r13p2.1337.ma>
2022-12-09 17:46:28 +0100

    add ex02 ffirst structure

---
+++ module_02/ex02/Fixed.cpp
@@ -0,0 +1,66 @@
+#include "./Fixed.hpp"
+
+Fixed::Fixed() : fixed_point(0)
+{
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int &num)
+{
+  std::cout << "Int constructor called" << std::endl;
+  fixed_point = num << 8;
+}
+
+Fixed::Fixed(const float &num)
+{
+  std::cout << "Float constructor called" << std::endl;
+  fixed_point = roundf(num * (1 << 8));
+}
+
+Fixed::~Fixed()
+{
+  std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &src)
+{
+  std::cout << "Copy assignment operator called" << std::endl;
+  fixed_point = src.getRawBits();
+  return *this;
+}
+
+void Fixed::setRawBits(const int set)
+{
+  fixed_point = set;
+}
+
+int Fixed::getRawBits(void) const
+{
+  // std::cout << "getRawBits member function called" << std::endl;
+  return (
+      fixed_point);
+}
+
+Fixed::Fixed(const Fixed &src)
+{
+
+  std::cout << "Copy constructor called" << std::endl;
+  *this = src;
+}
+
+int Fixed::toInt(void) const
+{
+  return (fixed_point >> 8);
+}
+
+float Fixed::toFloat(void) const
+{
+  float r = (float(fixed_point) / (1 << 8));
+  return r;
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &c)
+{
+  out << float(c.toFloat());
+  return (out);
+}
---
+++ module_02/ex02/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajaidi <ajaidi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 17:50:08 by ajaidi            #+#    #+#             */
+/*   Updated: 2022/12/09 17:05:33 by ajaidi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+  int fixed_point;
+  static const int fractional_bits = 8;
+
+public:
+  Fixed();
+  Fixed(const int&);
+  Fixed(const float&);
+  Fixed(const Fixed &);
+  ~Fixed();
+  Fixed &operator=(const Fixed &);
+  int getRawBits(void) const;
+  void setRawBits(int const);
+  float toFloat(void) const;
+  int toInt(void) const;  
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& c);
