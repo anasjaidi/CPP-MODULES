11638ea56cebb0d9e60520df8b7fe7d472ccc364
anas jaidi <anas.jaidi@icloud.com>
2023-04-01 18:08:56 +0000

    cpp09/ex00: parse data.csv file and fit it into map<string, float>

--- module_09/ex00/main.cpp
+++ module_09/ex00/main.cpp
@@ -5,7 +5,39 @@
 
 # include <iostream>
 # include <filesystem>
-#include <fstream>
+# include <fstream>
+# include <map>
+
+# include <sstream>
+
+
+std::pair<std::string, float> parse_line(std::string &line) {
+
+    float price;
+    std::istringstream ss(line.substr(line.find(',') + 1, line.length()));
+    std::string date;
+
+    date = line.substr(0, line.find(','));
+
+    ss >> price;
+
+    return std::make_pair(date, price);
+}
+
+std::map<std::string, float> store_data(std::fstream &infile) {
+
+    std::string line;
+    std::map<std::string, float> data;
+
+    std::getline(infile, line);
+
+    while (std::getline(infile, line)) {
+        std::pair<std::string, float> parsed_line = parse_line(line);
+        data[parsed_line.first] = parsed_line.second;
+    }
+
+    return data;
+}
 
 
 int main(int ac, char **av) {
@@ -35,5 +67,16 @@ int main(int ac, char **av) {
                 std::cout << "Error: could not open file." << std::endl,
                         1
         );
+
+    /*
+     * Everything works fine if the program rich this point
+     * **/
+
+    std::map<std::string, float> data = store_data(data_file);
+
+    for (std::map<std::string, float>::iterator it = data.begin(); it != data.end(); it++) {
+        std::cout << it->first << " | " << it->second << std::endl;
+    }
+
     return 0;
 }
