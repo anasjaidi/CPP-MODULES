63d51066f637ba5234db57b398da45078102d753
anas jaidi <anas.jaidi@icloud.com>
2023-04-02 14:55:05 +0000

    add append methods

--- module_09/ex02/pmerge_me.cpp
+++ module_09/ex02/pmerge_me.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "pmerge_me.hpp"
+
+
+void PmergeMe::add_num(int &num) {
+    list_add_num(num);
+    vec_add_num(num);
+}
+
+
+void PmergeMe::list_add_num(int &num) {
+    this->list.push_back(num);
+}
+
+
+void PmergeMe::vec_add_num(int &num) {
+    this->vec.push_back(num);
+}
